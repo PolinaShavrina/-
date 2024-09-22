fe8f047754bf619327204352920d0797a91a73ad
PolinaShavrina <polina.shavrina23.01.08.05@mail.ru>
2024-09-22 21:24:03 +0300

    Add files via upload

---
+++ фигура.cpp
@@ -0,0 +1,35 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, N = 50;
+    int c = N / 2;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            if (i <= c)
+            {
+               
+                if (j >= c - i && j <= c + i)
+                    cout << "+";
+                else
+                    cout << " ";
+            }
+            else
+            {
+            
+                if (j >= c + i - N + 1 && j <= c - i + N - 1)
+                    cout << "+";
+                else
+                    cout << " ";
+            }
+        }
+        cout << endl;
+    }
+  
+    return 0;
+    
+}
